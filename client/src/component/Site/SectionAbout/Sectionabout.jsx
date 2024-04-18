f9b5d09a350323da1288de8b343842a4537790fe
unknown <azizovagultakin@gmail.com>
2024-04-18 11:02:13 +0300

    section text

---
+++ client/src/component/Site/SectionAbout/Sectionabout.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import "../SectionAbout/Sectionabout.scss"
+const Sectionabout = () => {
+  return (
+    <>
+      <div className="div__text">
+        <h6>ABOUT US</h6>
+        <h1>Know Us Better
+</h1>
+
+<div className="common">
+<div className="common__table">
+<div className="common__table__button">
+<div><button>Web Design</button></div>
+<div><button>Grapihcs</button></div>
+<div><button>Web Coding</button></div>
+</div>
+
+<div className="common__table__list">
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Project Title</th>
+      <th scope="col"> Budget</th>
+      <th scope="col">Deadline</th>
+      <th scope="col"> Client</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+</div>
+</div>
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+      </div>
+    </>
+  )
+}
+
+export default Sectionabout
--- client/src/page/Site/Home/Home.jsx
+++ client/src/page/Site/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Cards from '../../../component/Site/Section2/Cards'
 import Carousel from '../../../component/Site/Section1/Carousel'
 import { Section3 } from '../../../component/Site/SectionThree/Section3'
+import Sectionabout from '../../../component/Site/SectionAbout/Sectionabout'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Carousel/>
       <Cards/>
       <Section3/>
+      <Sectionabout/>
     </div>
   )
 }
